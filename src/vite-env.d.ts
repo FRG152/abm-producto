647531a9c7ecea6371b4e5211ac0b5a6ff2e6c53
FRG152 <romanflavio3@hotmail.com>
2024-10-17 11:54:20 -0300

    change remotes url

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="vite/client" />
+declare module "design_system/Form" {
+  const Button: React.ComponentType<any>;
+  export default Button;
+}
+declare module "design_system/Button" {
+  const Form: React.ComponentType<any>;
+  export default Button;
+}
